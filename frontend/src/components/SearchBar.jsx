153fdaac676aac173cb26048e7bb7fbac6c1a733
CodeManBist <sagarbist372@gmail.com>
2026-03-13 16:25:47 +0530

    search bar ui created

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+function SearchBar({ search, setSearch }) {
+  return (
+    <input
+      type="text"
+      placeholder="Search by author..."
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      className="border p-2 w-full mb-4"
+    />
+  );
+}
+
+export default SearchBar;
